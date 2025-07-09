1635d9cd3e697991f4152e38fb08b73258b0c550
murilopbs <murilopbsouto@gmail.com>
2025-07-09 03:23:16 -0300

    Base completinha

--- base/base.ino
+++ base/base.ino
@@ -1,14 +1,21 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-// Credenciais da rede criada pela ESP8266
-const char* ssid = "ESP8266_Rede";
-const char* password = "12345678";
+// Login e senha da rede wifi
+const char* ssid = "G1_Ajax_Base";
+const char* password = "botafogo24";
 
 // Pino do motor
-const int PINO_MOTOR = D2;
+// const int ENA    = 5;  // GPIO 5 ou seja, D1
+// const int IN1    = 4;  // GPIO 4, ou seja, D2
+// const int IN2    = 0;  // GPIO 0, ou seja, D3
+//const int ENA    = 5;  // GPIO 5 ou seja, D1
+const int IN1    = 5;  // GPIO 4, ou seja, D2
+const int IN2    = 4;  // GPIO 0, ou seja, D3
+const int LED    = 2;  // GPIO 2, ou seja, D4
+const int BUZZER = 14; // GPIO 14, ou seja, D5
 
-// Servidor HTTP na porta 80 (servidor só pode ser acessado na rede da esp)
+// Servidor HTTP
 ESP8266WebServer server(80);
 
 // htmlzin cachorro
@@ -22,10 +29,7 @@ const char* htmlPage = R"rawliteral(
 <body>
   <h1>Controle o Pino D2</h1>
   <form action="/ligar" method="POST">
-    <button type="submit">Ligar</button>
-  </form>
-  <form action="/desligar" method="POST">
-    <button type="submit">Desligar</button>
+    <button type="submit">Decolar</button>
   </form>
 </body>
 </html>
@@ -33,8 +37,14 @@ const char* htmlPage = R"rawliteral(
 
 void setup() {
   Serial.begin(115200);
-  pinMode(PINO_RELE, OUTPUT);
-  digitalWrite(PINO_RELE, LOW); // Garante que comeca desligado
+  //pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  digitalWrite(IN1, LOW); // Garante que o motor vai começar desligado
+  digitalWrite(IN2, LOW); // Garante que o motor vai começar desligado
+  digitalWrite(LED, HIGH);
 
   // Cria a rede wifi
   WiFi.softAP(ssid, password);
@@ -49,9 +59,7 @@ void setup() {
 
   // Liga o motor
   server.on("/ligar", HTTP_POST, []() {
-    digitalWrite(PINO_MOTOR, HIGH);
-    delay(2000);
-    digitalWrite(PINO_MOTOR, LOW); // AQUI FALTA IMPLEMENTAR OS LEDS E OS KRL A 4 MAS PACIENCIA
+    ligaMotor();
     server.sendHeader("Location", "/");
     server.send(303); // Volta pra pagina principal
   });
@@ -59,8 +67,41 @@ void setup() {
 
   server.begin();
   Serial.println("Servidor HTTP iniciado"); // printa que iniciou pra gente nao ficar perdido
+  digitalWrite(LED, HIGH);
 }
 
 void loop() {
   server.handleClient();
+}
+
+void ligaMotor(){
+  r2d2();
+  //piscaLed(LED, 5);
+  // for(int relampagoMarquinhos = 0; relampagoMarquinhos < 255; relampagoMarquinhos++){
+  //   analogWrite(ENA, relampagoMarquinhos);
+  // }
+  digitalWrite(IN1, HIGH); // Garante que o motor vai terminar desligado
+  digitalWrite(IN2, LOW); // Garante que o motor vai terminar desligado
+}
+
+void piscaLed(int pino, int vezes){
+  for(int i = 0; i < vezes; i++){
+    digitalWrite(pino, LOW);
+    delay(500);
+    digitalWrite(pino, HIGH);
+    delay(500);
+  }
+}
+
+void r2d2() {
+  bool estadoLed = true;
+  for (int i = 0; i < 10; i++) {
+    int freq = random(500, 3000);     // Frequência entre 500Hz e 3000Hz
+    int dur = random(50, 200);        // Duração entre 50ms e 200ms
+    tone(BUZZER, freq, dur);       // Emite o som
+    digitalWrite(LED, estadoLed);
+    estadoLed = !estadoLed;
+    delay(dur + random(20, 100));     // Pausa entre os sons
+  }
+  noTone(BUZZER); // Para o som
 }
